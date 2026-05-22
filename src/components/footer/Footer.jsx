f3f477c4d81a351eca64e8c56bcacf5d72bae15f
Kressala <chdimaio@protonmail.com>
2026-05-22 15:06:25 +0200

    fix: fixed Footer structure

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,18 @@
-import styles from './Footer.module.scss';
-import Img from '../../assets/icons/instagram_icon.svg';
-
-
+import styles from "./Footer.module.scss";
+import Img from "../../assets/icons/instagram-icon.svg";
 
 const Footer = () => {
-    return <footer className={styles.footer}>
-        <div className={styles.footerContainer}>
-                <p className={styles.logoText}>GODDESSES TAROT</p>
-                <p className={styles.footerText}>© 2026. Designed with 💜 by Team 1</p>
-            <div className={styles.iconContainer}>
-                <img src={Img} alt="An icon that represents the Instagram app"></img>
-            </div>
-        </div>
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerContainer}>
+        <p className={styles.logoText}>GODDESSES TAROT</p>
+        <p className={styles.footerText}>© 2026. Designed with 💜 by Team 1</p>
+      </div>
+      <div className={styles.iconContainer}>
+        <img src={Img} alt="Icono que representa la app de Instagram"></img>
+      </div>
     </footer>
+  );
 };
 
-export default Footer
+export default Footer;
