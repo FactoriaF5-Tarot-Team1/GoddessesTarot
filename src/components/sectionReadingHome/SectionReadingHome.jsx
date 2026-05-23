a19506bdc4c94d9b18124b9279dc7e2cfed2fb84
Rosa Naharro Vaillant <rosana50factoria@gmail.com>
2026-05-23 17:16:02 +0200

    feat: refctoring component SectionReading

--- src/components/sectionReadingHome/SectionReadingHome.jsx
+++ src/components/sectionReadingHome/SectionReadingHome.jsx
@@ -1,15 +1,15 @@
 import styles from './SectionReadingHome.module.scss'
 import React from 'react'
 
-function SectionReadingHome({icon,title,description}) {
+function SectionReadingHome({icon,alt,title,description}) {
   return (
     <>
     <section className={styles.cardSectionReading}>
         {/* poner icon del componente de lectura */}
         <div className={styles.iconContainer}>
-            <img src={icon} alt="Icono de carta del pasado con un reloj"></img>
+            <img src={icon} alt={alt}></img>
         </div>     
-        <h2 className={styles.cardTitleSectionReading}>{title}</h2>
+        <h3 className={styles.cardTitleSectionReading}>{title}</h3>
         <hr />
         <p className={styles.cardDescriptionSectionReading}>{description}</p>
     </section>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-// import SectionReadingHome from '../../components/sectionReadingHome/SectionReadingHome'
+import React, { useState } from 'react'
+import SectionReadingHome from '../../components/sectionReadingHome/SectionReadingHome'
 
-import ArticleReadingCard from '../../components/sectionReadingHome/ArticleReadingCard'
 import styles from './Home.module.scss'
 
+import { arraySectionReadingHome } from '../../assets/data/dataGoodessTarot'; 
 function Home() {
+  // Guardamos el array en el estado de React
+  const [listSections, setListSections] = useState(arraySectionReadingHome);
+
   return (
     <>
-      <ArticleReadingCard/>
-      {/* <SectionReadingHome icon="./src/assets/icons/iconCardPast.svg" title="El Pasado" description="Las constantes que definieron tu trayectoria inicial" />
-      <SectionReadingHome icon="./src/assets/icons/iconCardPresent.svg" title="El Presente" description="La variable actual que requiere tu analisis consciente" />
-      <SectionReadingHome icon="./src/assets/icons/iconCardFuture.svg" title="El Futuro" description="El vector resultante de tus decisiones presentes" /> */}
+      <section className={styles.SectionReading}>
+        <h2 className={styles.TitleSectionReading}>Tirada de 3 Cartas</h2>
+        <p className={styles.ParagrahSectionReading}>TU PASADO, PRESENTE Y FUTURO</p>
+
+        {listSections.map((section,index) =>(
+          <SectionReadingHome icon={section.icon} alt={section.alt} title={section.title} description={section.description} />
+        ))
+        }
+        {/* <SectionReadingHome icon="./src/assets/icons/past2.svg" alt="Icono de carta del pasado con un reloj" title="El Pasado" description="Las constantes que definieron tu trayectoria inicial" />
+        <SectionReadingHome icon="./src/assets/icons/present2.svg" alt="Icono de carta del presente con una diana"title="El Presente" description="La variable actual que requiere tu analisis consciente" />
+        <SectionReadingHome icon="./src/assets/icons/future2.svg" alt="Icono de carta del futuro con 3 estrellas de 4 puntas y un rayo magico" title="El Futuro" description="El vector resultante de tus decisiones presentes" /> */}
+      </section>
     </>
   )
 }
