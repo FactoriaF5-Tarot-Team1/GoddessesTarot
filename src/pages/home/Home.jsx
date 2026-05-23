4a6e4b4a808b49568b0e7b8b3197ac740b9790d0
Rosa Naharro Vaillant <rosana50factoria@gmail.com>
2026-05-23 19:30:39 +0200

    style: add hover in card component SectionReadingHome

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -18,9 +18,6 @@ function Home() {
           <SectionReadingHome icon={section.icon} alt={section.alt} title={section.title} description={section.description} />
         ))
         }
-        {/* <SectionReadingHome icon="./src/assets/icons/past2.svg" alt="Icono de carta del pasado con un reloj" title="El Pasado" description="Las constantes que definieron tu trayectoria inicial" />
-        <SectionReadingHome icon="./src/assets/icons/present2.svg" alt="Icono de carta del presente con una diana"title="El Presente" description="La variable actual que requiere tu analisis consciente" />
-        <SectionReadingHome icon="./src/assets/icons/future2.svg" alt="Icono de carta del futuro con 3 estrellas de 4 puntas y un rayo magico" title="El Futuro" description="El vector resultante de tus decisiones presentes" /> */}
       </section>
     </>
   )
