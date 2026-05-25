3fe476929cabff1aa0da7d16a06a915bcda13723
AidaG91 <aida.garmus@gmail.com>
2026-05-25 13:34:35 +0200

    feat: install react-hook-form, improvejsx from form

--- src/components/readingForm/ReadingForm.jsx
+++ src/components/readingForm/ReadingForm.jsx
@@ -2,8 +2,20 @@ import React from "react";
 import starIcon from "../../assets/icons/stars.svg";
 import tinyStar from "../../assets/icons/tiny-star.svg";
 import styles from "./ReadingForm.module.scss";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function ReadingForm() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   const today = new Date();
   const formattedDay = today.toLocaleDateString("es-ES", {
     day: "2-digit",
@@ -13,7 +25,7 @@ function ReadingForm() {
 
   return (
     <section aria-labelledby="reading-title" className={styles.formSection}>
-      <form className={styles.form}>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
         <div className={styles.starWrapper}>
           <img
             src={starIcon}
@@ -35,19 +47,25 @@ function ReadingForm() {
           <input
             type="text"
             id="name"
-            name="name"
             placeholder="Tu nombre"
-            required
-            minLength={2}
             className={styles.input}
+            autoComplete="off"
+            {...register("name", {
+              required: "El nombre es obligatorio",
+              minLength: {
+                value: 2,
+                message: "El nombre debe tener al menos dos caracteres",
+              },
+            })}
           />
+          {errors.name && <p>{errors.name.message}</p>}
         </div>
 
         <p className={styles.date}>
           Fecha: <span className={styles.dateValue}>{formattedDay}</span>
         </p>
 
-        <button>Comenzar lectura</button>
+        <button type="submit">Comenzar lectura</button>
 
         <div className={styles.divider}>
           <img
@@ -57,7 +75,7 @@ function ReadingForm() {
           />
         </div>
 
-        <a href="/">Volver al inicio</a>
+        <Link to="/">Volver al inicio</Link>
       </form>
     </section>
   );
--- src/config/router.jsx
+++ src/config/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/home/Home";
 import ReadingName from "../pages/readingName/ReadingName";
