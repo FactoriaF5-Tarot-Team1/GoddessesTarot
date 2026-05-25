fd3b1cbf252d0a898effe255a3fd173e49d0ae6e
Ivanna Caraccio <ivacaraccio@hotmail.com>
2026-05-25 17:28:43 +0200

    feat: add card fan component with styles

---
+++ src/components/cardFan/CardFan.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { apiTarot } from "../../services/apiTarot";
+import cardBack from "../../assets/images/card-back.png";
+import styles from "./CardFan.module.scss";
+
+function CardFan({ id, selectCard, isDisabled }) {
+  const [isSelected, setIsSelected] = useState(false);
+
+  async function handleClick() {
+    if (isSelected || isDisabled) return;
+
+    try {
+      const api = apiTarot();
+      const cardData = await api.getCardById(id);
+      setIsSelected(true);
+      selectCard(cardData);
+    } catch (error) {
+      console.error("Error al obtener la carta:", error);
+    }
+  }
+
+  return (
+    <div
+      className={`${styles.cardFan} ${isSelected ? styles.selected : ""} ${isDisabled ? styles.disabled : ""}`}
+      onClick={handleClick}
+    >
+      <img src={cardBack} alt="carta del tarot boca abajo" />
+    </div>
+  );
+}
+
+export default CardFan;
