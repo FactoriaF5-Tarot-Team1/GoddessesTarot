78a3b5def13c8caf6218e025b27a6bdfcef9a382
unknown <elenaalmansacampos@gmail.com>
2026-05-26 08:51:01 +0200

    Add changes header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,79 @@
+import { useState } from "react";
 import "./Header.scss";
-import { HiOutlineMenuAlt3 } from "react-icons/hi";
+
+import starsIcon from "../../assets/icons/stars.png";
+import bookIcon from "../../assets/icons/book.png";
+
+import { HiOutlineMenuAlt3, HiX } from "react-icons/hi";
 
 export default function Header() {
-  return (
-    <header className="header">
-      {/* MENÚ */}
-      <button className="header__menu">
-        <HiOutlineMenuAlt3 />
-      </button>
+  const [menuOpen, setMenuOpen] = useState(false);
 
-      {/* LOGO */}
-      <h1 className="header__logo">
-        GODDESSES TAROT
-      </h1>
-    </header>
+  return (
+    <>
+      {/* =========================
+          HEADER
+      ========================= */}
+      <header className="header">
+        {/* IZQUIERDA */}
+        <div className="header__left">
+          {/* HAMBURGUESA */}
+          <button className="header__menu" onClick={() => setMenuOpen(true)}>
+            <HiOutlineMenuAlt3 />
+          </button>
+
+          {/* LOGO */}
+          <h1 className="header__logo">GODDESSES TAROT</h1>
+        </div>
+
+        {/* NAV DESKTOP */}
+        <nav className="header__nav">
+          <a href="/" onClick={() => setMenuOpen(false)}>
+            <img src={starsIcon} alt="stars icon" className="menu__icon" />
+            <span>Inicio</span>
+            <img src={bookIcon} alt="book icon" className="menu__icon" />
+          </a>
+
+          <a href="/">
+            <img src={starsIcon} alt="stars icon" className="header__icon" />
+            Lecturas anteriores
+          </a>
+        </nav>
+      </header>
+
+      {/* =========================
+          OVERLAY MENU (MOBILE)
+      ========================= */}
+      <div className={`menu ${menuOpen ? "active" : ""}`}>
+        {/* BOTÓN CERRAR */}
+        <button className="menu__close" onClick={() => setMenuOpen(false)}>
+          <HiX />
+        </button>
+
+        {/* TÍTULO */}
+        <h2 className="menu__title">PORTAL CÓSMICO</h2>
+
+        <div className="menu__line"></div>
+
+        {/* LINKS MOBILE */}
+        <nav className="menu__nav">
+          {/* Inicio con icono debajo */}
+          <a href="/" className="menu__link--active">
+            Inicio
+            <img src={bookIcon} alt="book icon" className="menu__icon" />
+          </a>
+
+          <a href="/">
+            Lecturas anteriores
+            <img src={starsIcon} alt="stars icon" className="menu__icon" />
+          </a>
+        </nav>
+
+        {/* DECORACIÓN INFERIOR */}
+        <div className="menu__bottom">
+          <span></span>✧<span></span>
+        </div>
+      </div>
+    </>
   );
 }
