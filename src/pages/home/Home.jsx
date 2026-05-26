9e670317065418ea304ed566df9b215945aa51db
Ivanna Caraccio <ivacaraccio@hotmail.com>
2026-05-26 15:55:35 +0200

    feat: add CardSlot component structure for past, present and future section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Reading from "../reading/Reading";
 
 function Home() {
   return (
     <>
-    
+    <Reading></Reading>
     </>
   );
 }
--- src/pages/reading/Reading.jsx
+++ src/pages/reading/Reading.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Fan from "../../components/fan/Fan";
 import TurnIndicator from "../../components/turn-indicator/TurnIndicator";
+import CardSlot from "../../components/card-slot/CardSlot";
 import useFanCards from "../../hooks/useFanCards";
 
 function Reading() {
-  const { shuffledIds, selectCard, selectedCards, currentTurn } = useFanCards();
+  const { shuffledIds, selectCard, selectedCards, currentTurn, openModal } =
+    useFanCards();
 
   return (
     <>
@@ -14,6 +16,15 @@ function Reading() {
         selectedCards={selectedCards}
       ></Fan>
       <TurnIndicator currentTurn={currentTurn} />
+      <div>
+        <CardSlot turn="pasado" card={selectedCards[0]} openModal={openModal} />
+        <CardSlot
+          turn="presente"
+          card={selectedCards[1]}
+          openModal={openModal}
+        />
+        <CardSlot turn="futuro" card={selectedCards[2]} openModal={openModal} />
+      </div>
     </>
   );
 }
