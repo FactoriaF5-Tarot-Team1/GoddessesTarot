72c5c8293e03ef7823a5ae8c6d2a0766ac3a5f11
Kressala <chdimaio@protonmail.com>
2026-05-28 13:14:14 +0200

    fix: Women Section ready to merge

--- src/components/women-section/WomenSection.jsx
+++ src/components/women-section/WomenSection.jsx
@@ -5,34 +5,44 @@ import starsIcon from "../../assets/icons/stars.png";
 function WomenSection() {
   return (
     <section className={styles.womenSection}>
-      <div className={styles.tarotDeckContent}>
-        <div className={styles.stemTarotDivider}>
-          <img src={flaskIcon} alt="Icono de una ampolla de laboratorio"></img>
-          <p>EL TAROT STEM</p>
+      <article className={styles.womenSectionContainer}>
+        <div className={styles.adaLovelaceContainer}>
+          <img
+            className={styles.adaLovelaceImage}
+            src={womenSectionImg}
+            alt="Imágen de una mujer con un abanico de cartas en la mano"
+          ></img>
+          <div className={styles.cardOverlayText}>
+            <h3>Ada Lovelace</h3>
+            <p>La Suma Sacerdotisa del Código</p>
+          </div>
         </div>
-        <div className={styles.tarotDeckDescription}>
-          <h2>Las mujeres que lo representan</h2>
-          <p>
-            Cada arcano en nuestro mazo ha sido reimaginado a través de las
-            mentes brillantes que forjaron la ciencia y la tecnología. Desde las
-            astrónomas que cartografiaron el infinito hasta las matemáticas que
-            descifraron el lenguaje de la creación.
-          </p>
+        <div className={styles.tarotDeckContent}>
+          <div className={styles.stemTarotDivider}>
+            <img
+              src={flaskIcon}
+              alt="Icono de una ampolla de laboratorio"
+            ></img>
+            <p>EL TAROT STEM</p>
+          </div>
+          <div className={styles.tarotDeckDescription}>
+            <h2>Las mujeres que lo representan</h2>
+            <p>
+              Cada arcano en nuestro mazo ha sido reimaginado a través de las
+              mentes brillantes que forjaron la ciencia y la tecnología. <br></br>
+              Desde las astrónomas que cartografiaron el infinito hasta las
+              matemáticas que descifraron el lenguaje de la creación.
+            </p>
+          </div>
         </div>
-      </div>
-      <div className={styles.adaLovelaceContainer}>
-      <img
-        className={styles.adaLovelaceImage}
-        src={womenSectionImg}
-        alt="Imágen de una mujer con un abanico de cartas en la mano"
-      ></img>
-      <div className={styles.cardOverlayText}>
-        <h3>Ada Lovelace</h3>
-        <p>La Suma Sacerdotisa del Código</p>
-      </div>
-      </div>
+      </article>
+
       <div className={styles.starsContainer}>
-      <img className={styles.starsIcon} src={starsIcon} alt="Icono de decoración con tres estrellas"></img>
+        <img
+          className={styles.starsIcon}
+          src={starsIcon}
+          alt="Icono de decoración con tres estrellas"
+        ></img>
       </div>
     </section>
   );
